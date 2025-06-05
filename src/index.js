644f9e86eab8f7149f4c8b328703cacaca1aa1f2
Héctor Sharpe <cobranzaarriendos@gmail.com>
2025-06-05 18:06:06 -0400

    vuelta al comienzo

--- src/index.js
+++ src/index.js
@@ -1,39 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ChatBot from './components/ChatBot';
-import VersionChecker from './components/VersionChecker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <>
-    <VersionChecker />
-    <ChatBot />
-  </>
-);
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then(registration => {
-        console.log('✅ Service Worker registrado con éxito:', registration);
-
-        registration.onupdatefound = () => {
-          const installingWorker = registration.installing;
-          installingWorker.onstatechange = () => {
-            if (installingWorker.state === 'installed') {
-              if (navigator.serviceWorker.controller) {
-                // Hay una nueva versión disponible
-                console.log('🚨 Nueva versión disponible, avisar desde VersionChecker');
-                window.dispatchEvent(new Event('new-version-available'));
-              }
-            }
-          };
-        };
-      })
-      .catch(error => {
-        console.error('❌ Falló el registro del Service Worker:', error);
-      });
-  });
-}
+root.render(<ChatBot />);
